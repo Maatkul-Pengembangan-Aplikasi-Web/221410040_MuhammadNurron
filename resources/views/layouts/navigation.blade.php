a2abe4ac5c53f3e9460461852b84aa9c72f2565e
muhammadnurron <mhmdnurron@gmail.com>
2024-11-03 19:39:34 +0700

    +halaman program studi

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('/Prodi')" :active="request()->routeIs('/Prodi')">
+                        {{ __('Program Studi') }}
+                    </x-nav-link>
                 </div>
             </div>
 
